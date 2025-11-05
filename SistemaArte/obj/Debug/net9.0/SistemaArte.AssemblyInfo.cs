64fe5899b3d00d4787c937de09b0ae543da5b3cf
jenifer-lee1 <jenifer23500@gmail.com>
2025-11-05 17:10:37 -0300

    e

--- SistemaArte/obj/Debug/net9.0/SistemaArte.AssemblyInfo.cs
+++ SistemaArte/obj/Debug/net9.0/SistemaArte.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaArte")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a69a82f3e46c1af3fb145185df826c1c138178c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+940ce4fc24cada3a093fad7e90452cb5d6e09cd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaArte")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaArte")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
